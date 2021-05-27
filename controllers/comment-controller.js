aea94500f581aed06abbc995dad479f306831cfa
Megs <Megs.close@gmail.com>
2021-05-27 12:30:12 -0700

    feature finished

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -13,7 +13,7 @@ const commentController = {
                 );
             })
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
+                if (!dbPizzaData) {
                     res.status(400).json({ message: "No pizza found with this id!" });
                     return;
                 }
@@ -21,11 +21,38 @@ const commentController = {
             })
             .catch(err => res.json(err));
     },
+    // add a reply
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: "No pizza found with this id!" });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.json(err));
+    },
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
             .then(deletedComment => {
-                if(!deletedComment) {
+                if (!deletedComment) {
                     return res.status(404).json({ message: "No comment with this id!" });
                 }
                 return Pizza.findOneAndUpdate(
